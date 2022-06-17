d50d8f6a5531555b13ecf30ff7e9c53c8b505358
kimjy-par <kimjy.par@gmail.com>
2022-06-17 17:31:03 +0900

    Feat: add Ecto and context

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -8,6 +8,31 @@ defmodule HelloWeb.Router do
     plug :put_root_layout, {HelloWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
+    plug :fetch_current_cart
+  end
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+    end
+  end
+
+  alias Hello.ShoppingCart
+
+  def fetch_current_cart(conn, _opts) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
+    end
   end
 
   pipeline :api do
@@ -20,6 +45,7 @@ defmodule HelloWeb.Router do
     resources "/reviews", ReviewController
  
     get "/", PageController, :index
+    resources "/products", ProductController
     get "/redirect_test", PageController, :redirect_test
     resources "/users", UserController do
         resources "/posts", PostController
@@ -27,6 +53,10 @@ defmodule HelloWeb.Router do
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
 
+    resources "/cart_items", CartItemController, only: [:create, :delete]
+
+    get "/cart", CartController, :show
+    put "/cart", CartController, :update
     
     #resources "/posts", PostController, only: [:index, :show]
     #resources "/comments", CommentController, except: [:delete]
